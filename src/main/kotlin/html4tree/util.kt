3e3b45a664e0b35dfce7ed18bb48dc23f8684905
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-06-29 07:44:39 +0000

    🛡️ Sentinel: [CRITICAL] XSS 취약점 및 구버전 빌드 환경 수정 (주석 응답 완료)

--- src/main/kotlin/html4tree/util.kt
+++ src/main/kotlin/html4tree/util.kt
@@ -15,18 +15,9 @@ class LinkedList {
             last = Entry(lle.file, lle.level, null)
             first = last
         } else {
-            val nextEntry = Entry(lle.file, lle.level, null)
-            val currentFirst = first
-            if (currentFirst == null) {
-                var currentLast = last!!
-                while (currentLast.next != null) {
-                    currentLast = currentLast.next!!
-                }
-                currentLast.next = nextEntry
-            } else {
-                currentFirst.next = nextEntry
-            }
-            first = nextEntry
+            first?.next = Entry(lle.file, lle.level, null)
+            first = first?.next
+            first?.next = null
         }
     }
 
@@ -39,7 +30,7 @@ class LinkedList {
         if(l == null){
             return null
         } else {
-            l.next = null
+	        l.next = null
             return LinkedListEntry(l.data, l.level)
         }
     }
