8a44e643273600ba0e2b6e8f6f84705a7d95fa57
Seongho Bae <me@seonghobae.me>
2026-08-06 11:28:37 +0900

    test(security): make identity regression compile on Kotlin 1.3

--- src/test/kotlin/html4tree/CrawlDirectoriesIdentityRegressionTest.kt
+++ src/test/kotlin/html4tree/CrawlDirectoriesIdentityRegressionTest.kt
@@ -59,7 +59,7 @@ class CrawlDirectoriesIdentityRegressionTest {
                         }
                         else -> FileIdentity(null, false)
                     }
-                },
+                }
             )
 
             assertEquals(listOf(root), processed)
