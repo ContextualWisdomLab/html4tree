7e2861ff31ad7474bb2bb9f94d0394cdd1068137
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-08-24 03:35:22 +0000

    Use visually-hidden span for nav label to support browser translation

--- src/main/kotlin/html4tree/main.kt
+++ src/main/kotlin/html4tree/main.kt
@@ -427,7 +427,8 @@ fun process_dir(curr_dir: File, excludeSet: Set<String>? = null, dirFiles: Array
      <body>
        <main>
          <h1>${directoryName.escapeHtml()}</h1>
-         <nav aria-label="디렉토리 목록">
+         <nav aria-labelledby="nav-label">
+         <span id="nav-label" class="visually-hidden">디렉토리 목록</span>
          <ul role="list">
             <li><a class="dir-link" href="./.." title="상위 디렉토리로 이동"><span class="icon" aria-hidden="true">&#x21B0;</span> <span aria-hidden="true">..</span> <span class="visually-hidden">상위 디렉토리로 이동</span></a></li>
 """ 
