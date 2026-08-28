a1096a113a5eff0520bfb61a06587031e5f342ac
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-08-28 03:41:17 +0000

    🎨 Palette: [UX improvement] 링크 요소에 :active 상태 추가

--- src/main/kotlin/html4tree/main.kt
+++ src/main/kotlin/html4tree/main.kt
@@ -56,10 +56,7 @@ a:hover, a:focus-visible {
   outline: 2px solid #0969da;
   outline-offset: -2px;
 }
-a:active {
-  background-color: #eaeef2;
-}
-a:hover span:last-child, a:focus-visible span:last-child, a:active span:last-child {
+a:hover span:last-child, a:focus-visible span:last-child {
   text-decoration: underline;
 }
 @media (prefers-reduced-motion: reduce) {
@@ -100,9 +97,6 @@ li + li {
   a:hover, a:focus-visible {
     background-color: #161b22;
     outline-color: #58a6ff;
-  }
-  a:active {
-    background-color: #21262d;
   }
   li + li {
     border-top-color: #21262d;
