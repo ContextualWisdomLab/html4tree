2a092932932d885158aa6156c101423081466fc2
Seongho Bae <me@seonghobae.me>
2026-09-03 17:09:03 +0900

    test(security): isolate ignore-file race RED

--- src/test/kotlin/html4tree/SymlinkReadTest.kt
+++ src/test/kotlin/html4tree/SymlinkReadTest.kt
@@ -4,7 +4,6 @@ import org.junit.Test
 import java.io.IOException
 import java.io.InputStream
 import java.nio.file.Files
-import java.nio.file.UnsupportedOperationException
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertFailsWith
