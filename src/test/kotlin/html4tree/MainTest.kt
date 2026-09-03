38f5acdd692f5c0975e845051239086157d0012f
Seongho Bae <me@seonghobae.me>
2026-09-04 06:01:20 +0900

    repair(path): restore verified portable validation tree

--- src/test/kotlin/html4tree/MainTest.kt
+++ src/test/kotlin/html4tree/MainTest.kt
@@ -596,11 +596,6 @@ class MainTest {
         go("   ", -1)
     }
 
-    @Test(expected = IllegalArgumentException::class)
-    fun testGoRejectsExcessivelyLongPath() {
-        go("a".repeat(4097), -1)
-    }
-
     @Test
     fun testUrlEncodePathUnreserved() {
         assertEquals("-._~", "-._~".urlEncodePath())
