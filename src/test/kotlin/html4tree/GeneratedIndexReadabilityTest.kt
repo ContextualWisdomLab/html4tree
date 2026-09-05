a5516fcefd941ea109c380b5a93e065a4586c9a9
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-05 06:45:36 +0000

    🎨 Palette: [UX 개선] 링크에 :active 상태 추가

--- src/test/kotlin/html4tree/GeneratedIndexReadabilityTest.kt
+++ src/test/kotlin/html4tree/GeneratedIndexReadabilityTest.kt
@@ -44,7 +44,7 @@ class GeneratedIndexReadabilityTest {
         )
 
         val generatedHtml = generatedHtml()
-        val parentIndex = generatedHtml.indexOf("<span aria-hidden=\"true\">..</span>")
+        val parentIndex = generatedHtml.indexOf("<span class=\"entry-name\" aria-hidden=\"true\">..</span>")
         val firstIndex = generatedHtml.indexOf("alpha.txt")
         val middleIndex = generatedHtml.indexOf("middle.txt")
         val lastIndex = generatedHtml.indexOf("zulu.txt")
