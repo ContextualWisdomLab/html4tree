4fa04dc0a54ecd2dace1c32fcf84c4d2ffb71e10
Seongho Bae <me@seonghobae.me>
2026-09-08 09:04:23 +0900

    chore: remove branch-local memory probe

--- test-memory.kt
+++
@@ -1,9 +0,0 @@
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.LinkOption
-
-fun main() {
-    val ignore_file = File(".html4ignore")
-    ignore_file.createNewFile()
-    println(ignore_file.isFile)
-}
