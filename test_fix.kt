7a8ae586affd8ab8dd5f9d4746b020294e84ee2e
Seongho Bae <me@seonghobae.me>
2026-09-08 09:04:29 +0900

    chore: remove branch-local fix probe

--- test_fix.kt
+++
@@ -1,9 +0,0 @@
-import java.nio.file.Files
-import java.io.File
-
-fun main() {
-    val tempDir = Files.createTempDirectory("test").toFile()
-    val ignoreDir = File(tempDir, ".html4ignore")
-    ignoreDir.mkdir()
-    println("Done")
-}
