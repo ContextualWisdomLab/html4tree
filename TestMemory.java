1f42896a902bf88f551072aa852246362b47fb54
Seongho Bae <me@seonghobae.me>
2026-09-08 09:04:06 +0900

    chore: remove branch-local scratch probe

--- TestMemory.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public class TestMemory {
-    public static void main(String[] args) throws Exception {
-        File ignore_file = new File(".html4ignore");
-        ignore_file.createNewFile();
-        System.out.println(ignore_file.isFile());
-    }
-}
