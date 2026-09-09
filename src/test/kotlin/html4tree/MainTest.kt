4932d38f25829e82b1c2abf59f28864f84a9cc89
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-09 04:05:02 +0000

    Revert changes as requested

--- src/test/kotlin/html4tree/MainTest.kt
+++ src/test/kotlin/html4tree/MainTest.kt
@@ -596,12 +596,6 @@ class MainTest {
         go("   ", -1)
     }
 
-    @Test(expected = IllegalArgumentException::class)
-    fun testGoPathTooLong() {
-        val longPath = "a".repeat(4097)
-        go(longPath, -1)
-    }
-
     @Test
     fun testUrlEncodePathUnreserved() {
         assertEquals("-._~", "-._~".urlEncodePath())
