8bc21894b00efb32b9d013350222326e9e21e48b
Seongho Bae <me@seonghobae.me>
2026-09-20 03:12:41 +0900

    test(ignore): assert malformed byte result

--- src/test/kotlin/html4tree/IgnoreFileDosTest.kt
+++ src/test/kotlin/html4tree/IgnoreFileDosTest.kt
@@ -1,22 +1,21 @@
 package html4tree
 
 import org.junit.Test
+import org.junit.Assert.assertTrue
 import java.io.File
 import java.nio.file.Files
 
 class IgnoreFileDosTest {
     @Test
-    fun testProcessIgnoreFileWithException() {
+    fun testProcessIgnoreFileReplacesMalformedUtf8WithoutCrashing() {
         val dir = Files.createTempDirectory("test-ignore-dos").toFile()
         val ignoreFile = File(dir, ".html4ignore")
         // Write invalid UTF-8 bytes
         ignoreFile.writeBytes(byteArrayOf(0xFF.toByte(), 0xFE.toByte(), 0xFF.toByte()))
 
         try {
-            process_ignore_file(dir)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            org.junit.Assert.fail("process_ignore_file should not crash: " + e)
+            val excludedNames = process_ignore_file(dir)
+            assertTrue(excludedNames.contains(".html4ignore"))
         } finally {
             dir.deleteRecursively()
         }
