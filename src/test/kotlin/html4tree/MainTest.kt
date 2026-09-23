621e3ccad283937a042ddfb9436bd6fc638e44c7
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-23 03:26:39 +0000

    🛡️ Sentinel: [보안 개선]
    최상위 디렉토리 경로 길이에 대한 제한을 추가하여 DoS를 방지합니다.

--- src/test/kotlin/html4tree/MainTest.kt
+++ src/test/kotlin/html4tree/MainTest.kt
@@ -128,6 +128,13 @@ class MainTest {
     }
 
     @Test
+    fun testGoRejectsExtremelyLongPath() {
+        assertFailsWith<IllegalArgumentException> {
+            go("a".repeat(5000), -1)
+        }
+    }
+
+    @Test
     fun testGoIgnoresHiddenFilesAndDirectories() {
         val hiddenFile = File(tempDir, ".hidden_file.txt")
         hiddenFile.createNewFile()
