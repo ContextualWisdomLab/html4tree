e7ccbcc12f8bf81abf578333c97afa881e74936f
Seongho Bae <me@seonghobae.me>
2026-09-27 02:54:58 +0900

    test: keep sensitive-extension contract behavioral

--- src/test/kotlin/html4tree/HiddenFileSecurityTest.kt
+++ src/test/kotlin/html4tree/HiddenFileSecurityTest.kt
@@ -50,13 +50,4 @@ class HiddenFileSecurityTest {
         }
     }
 
-    @Test
-    fun defaultSensitiveExtensionsIsAnArray() {
-        val arrayField = Class.forName("html4tree.Constants").getDeclaredField("defaultSensitiveExtensions")
-        arrayField.isAccessible = true
-        val extensionsArray = arrayField.get(null)
-        assertTrue(extensionsArray is Array<*>, "defaultSensitiveExtensions should be an array for optimized iteration")
-        assertTrue((extensionsArray as Array<*>).size > 0, "defaultSensitiveExtensions array must not be empty")
-    }
-
 }
