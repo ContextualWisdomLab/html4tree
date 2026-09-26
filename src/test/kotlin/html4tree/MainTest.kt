f88f4f2622c22e3a0f8bc88cbbfe8865cd4a97c7
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-09-26 03:38:09 +0000

    🛡️ Sentinel: [보안 개선] 디렉토리 경로 길이 제한 추가

--- src/test/kotlin/html4tree/MainTest.kt
+++ src/test/kotlin/html4tree/MainTest.kt
@@ -83,6 +83,12 @@ class MainTest {
     }
 
     @Test(expected = IllegalArgumentException::class)
+    fun testGoPathTooLong() {
+        val longPath = "a".repeat(4097)
+        go(longPath, -1)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
     fun testGoInvalidDir() {
         go("non_existent_directory", -1)
     }
